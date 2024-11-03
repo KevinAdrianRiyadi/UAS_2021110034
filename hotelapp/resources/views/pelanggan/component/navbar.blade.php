acdb5997c0ee0587d793b7d9f41ea581e52aa457
kevinar <147068169+KevinAdrianRiyadi@users.noreply.github.com>
2024-11-03 19:54:17 +0700

    add frontend for project

---
+++ hotelapp/resources/views/pelanggan/component/navbar.blade.php
@@ -0,0 +1,31 @@
+<nav class="navbar navbar-dark navbar-expand-lg" style="background-color: #025464">
+    <div class="container">
+      <a class="navbar-brand" href="#">Hotel</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end gap-4" id="navbarNav">
+        <ul class="navbar-nav gap-4">
+          <li class="nav-item">
+            <a class="nav-link fs-5 active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link fs-5 " href="#">Shop</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link fs-5 " href="#">Contact Us</a>
+          </li>
+        </ul>
+         <div class="d-flex gap-4 align-items-center">
+            <button class="btn btn-success" type="button">Login | Register</button>
+            <div class="notif "></div>
+            <a href="#" class="fs-5">
+              <div class="circle"></div>
+                <i class="fa-solid fa-bag-shopping">
+
+                </i>
+            </a>
+         </div>
+      </div>
+    </div>
+  </nav>
---
+++ hotelapp/resources/views/pelanggan/layout/index.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="{{asset('css/customer.css')}}">
+
+    <title> Hotel | {{$title}} </title>
+</head>
+<body>
+    <main>
+        <header>
+            @include('pelanggan.component.navbar')
+        </header>
+        <section>
+            tes
+        </section>
+        <footer>
+            <div class="container">
+                <div class="d-flex">
+                    <div class="title-left">
+                        <div class="header-title">
+                            HOTEL X
+                        </div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, tempora totam omnis quo, eligendi repellendus fugit deserunt illum temporibus non perferendis nulla quos, hic quod magnam nihil blanditiis quis voluptates?</p>
+
+                    </div>
+                    <div class="title-middle">
+                        <div class="header-title">
+                            Tentang Kami
+                        </div>
+                        <ul>
+                            <li>
+                                <address>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla amet necessitatibus optio itaque nam pariatur magni dignissimos odit autem dicta, delectus quas inventore vel facilis expedita corrupti voluptatum, obcaecati deleniti.</address>
+                                </li>
+                                <li>
+                                    <p>4354354354</p>
+                                </li>
+                                <li>
+                                     4534535345   
+                                </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="title-sosmed">
+                    <i class="fa-brands fa-instagram"></i>
+                    <i class="fa-brands fa-square-facebook"></i>
+                    <i class="fa-brands fa-whatsapp"></i>                </div>
+            </div>
+        </footer>
+    </main>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>  
+</html>
